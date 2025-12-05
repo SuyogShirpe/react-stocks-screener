cf729dcd48d9b87d8b6930a243e3f3a2a8174c9f
Suyog Shirpe <shirpesuyog@gmail.com>
2025-12-05 20:04:33 +0530

    added style

--- src/components/SearchDisplay.jsx
+++ src/components/SearchDisplay.jsx
@@ -3,10 +3,12 @@ export default function SearchDisplay({ searchResults }) {
     return <div>No results found.</div>;
   }
   return (
-    <ul>
+    <ul className="list-group mx-auto mt-3" style={{ width: '30%' }}>
       {searchResults.map((result) => (
-        <li key={result.symbol} className="list-group-item">
-          {result.description} - ({result.displaySymbol})
+        <li key={result.symbol} className="list-group-item d-flex justify-content-between align-items-center"
+          style={{cursor:"pointer"}}>
+          <span>{result.description}</span>
+          <span className="text-muted">{result.displaySymbol}</span>
         </li>
       ))}
     </ul>
