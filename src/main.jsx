3708485e80973b54fe0e338397d5d8b0a0af6161
Suyog Shirpe <shirpesuyog@gmail.com>
2025-12-08 22:07:13 +0530

    .

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter, createBrowserRouter, createRoutesFromElements, Route, Router, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
 import Home from './components/Home.jsx';
-import Wishlist from './components/Wishlist.jsx';
+import Watchlist from './components/Watchlist.jsx';
 import About from './components/About.jsx';
 import Contact from './components/Contact.jsx';
 import StockDetails from './components/StockDetails.jsx';
@@ -14,7 +14,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />} >
       <Route path='' element={<Home />} />
-      <Route path='/wishlist' element={<Wishlist />} />
+      <Route path='/watchlist' element={<Watchlist />} />
       <Route path='/about' element={<About />} />
       <Route path='/contact' element={<Contact />} />
       <Route path="/stock/:ticker" element={<StockDetails />} /> 
