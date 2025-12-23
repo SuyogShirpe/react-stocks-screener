e9810f9960aef54ab487f156e87daa51b610db86
Suyog Shirpe <shirpesuyog@gmail.com>
2025-12-23 22:54:26 +0530

    added try and catch block

--- src/custom_hooks/useRandProfQtApi.jsx
+++ src/custom_hooks/useRandProfQtApi.jsx
@@ -6,39 +6,57 @@ export default function useRandProfQtApi(tickers = []) {
 
   const [stocks, setStocks] = useState([]);
   const [isLoading, setIsLoading] = useState(Boolean(tickers.length));
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     if (!tickers || tickers.length == 0) {
       setStocks([]);
       setIsLoading(false);
+      setError(null);
       return;
     }
 
     const fetchStocks = async () => {
       setIsLoading(true);
+      setError(null);
 
-      const result = await Promise.all(
-        tickers.map(async (ticker) => {
-          const [profileRes, quoteRes] = await Promise.all([
-            fetch(
-              `https://finnhub.io/api/v1/stock/profile2?symbol=${ticker}&token=${FINNHUB}`
-            ),
-            fetch(
-              `https://finnhub.io/api/v1/quote?symbol=${ticker}&token=${FINNHUB}`
-            ),
-          ]);
-          const profileData = await profileRes.json();
-          const quoteData = await quoteRes.json();
+      try {
+        const result = await Promise.all(
+          tickers.map(async (ticker) => {
+            try {
+            const [profileRes, quoteRes] = await Promise.all([
+              fetch(
+                `https://finnhub.io/api/v1/stock/profile2?symbol=${ticker}&token=${FINNHUB}`
+              ),
+              fetch(
+                `https://finnhub.io/api/v1/quote?symbol=${ticker}&token=${FINNHUB}`
+              ),
+            ]);
 
-          return { profile: profileData, quote: quoteData };
-        })
-      );
-      setStocks(result);
-      setIsLoading(false);
+            if (!profileRes.ok || !quoteRes.ok) {
+              return null;
+            }
+            const profileData = await profileRes.json();
+            const quoteData = await quoteRes.json();
+
+            return { profile: profileData, quote: quoteData };
+          } catch {
+            return null;
+          }
+          })
+        );
+      
+        setStocks(result.filter(stock => stock !== null));
+      } catch (error) {
+        setError(error.message ||"Something went wrong. Please try again later.");
+        setStocks([]);
+      } finally {
+        setIsLoading(false);
+      }
     };
     fetchStocks();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [tickers]);
 
-  return { stocks, isLoading };
+  return { stocks, isLoading, error };
 }
